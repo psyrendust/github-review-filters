6c15dc472b588b19abbaf325191e02f9aa92a2c7
Larry Gordon <larry.gordon@xero.com>
2014-09-16 14:26:12 -0700

    New UI and filters for pulls list

--- GitHub-Review-Filters/content/inject.js
+++ GitHub-Review-Filters/content/inject.js
@@ -1,67 +1,88 @@
 function GhHelper(){
-  $('div.wrapper > div.header').css({
-    'position': 'fixed',
-    'width': '100%',
-    'z-index': '100'
-  }).append('<div class="container clearfix" id="gh-helper"><ul class="top-nav"></ul></div>');
-  $('div.wrapper > div.site').css({
-    'padding-top': '80px'
-  });
   var self = this;
-  var $ghHelper = $('#gh-helper');
-  var $ghHelperNav = $('#gh-helper > ul.top-nav');
-  var $allItems = $('div.meta[data-path]');
+  var user = $('#user-links > li > a').text().trim();
+  var isPulls = window.location.pathname.split('/')[3] === 'pulls';
+  var $wrapper = $('body > div.wrapper');
+  var $header = $('div.wrapper > div.header');
+  var $content = $('div.wrapper > div.site');
+  $wrapper.addClass('gh-helper');
+  $header.after('<div id="ghHelperNav" class="'+$header.attr('class')+'"><div class="container clearfix"><ul class="top-nav"></ul></div></div>');
+  var $ghHelper = $('.gh-helper');
+  var $ghHelperNav = $('#ghHelperNav');
+  var $ghHelperNavUl = $ghHelperNav.find('div > ul.top-nav');
+  var $allItems = isPulls ? $('ul.pulls-list-group > li.list-group-item > .list-group-item-name') : $('div.meta[data-path]');
   var typeIsVisible = {};
+  var $ghLegend;
   self.addLink = function(fn, label) {
     $('<li><a>'+label+'</a></li>')
     .click(function() {
       self[fn]();
     })
-    .appendTo($ghHelperNav);
+    .appendTo($ghHelperNavUl);
   };
   self.addToggleLink = function(type) {
-    $('<li class="toggleLink selected"><a>.'+type+' ('+fileTypes[type].length+')</a></li>')
+    var label = (isPulls?'':'.') + type;
+    $('<li class="toggleLink selected"><a>'+label+' ('+fileTypes[type].length+')</a></li>')
     .click(function() {
       if (typeIsVisible[type]) {
         fileTypes[type].forEach(function(item) {
-          item.parent().hide();
+          if (isPulls) {
+            item.parent().parent().parent().hide();
+          } else {
+            item.parent().hide();
+          }
         });
         typeIsVisible[type] = false;
         $(this).removeClass('selected');
       } else {
         fileTypes[type].forEach(function(item) {
-          item.parent().show();
+          if (isPulls) {
+            item.parent().parent().parent().show();
+          } else {
+            item.parent().show();
+          }
         });
         typeIsVisible[type] = true;
         $(this).addClass('selected');
       }
     })
-    .appendTo($ghHelperNav);
+    .appendTo($ghHelperNavUl);
+  };
+  self.addLegend = function() {
+    $('<li><a class="octicon octicon-info"></a><div id="ghLegend"><div class="arrow"></div><div class="gh-legend-title">GitHub Review Filters</div><div class="gh-legend-label"><div>Visible</div><div>Hidden</div></div><div class="gh-legend-img"></div></div></li>')
+    .appendTo($ghHelperNavUl);
+    $ghLegend = $ghHelperNavUl.find('#ghLegend');
   };
   self.addDivider = function() {
     $('<div class="divider-vertical"></div>')
-    .appendTo($ghHelperNav);
+    .appendTo($ghHelperNavUl);
   };
   self.showAll = function() {
     $($allItems).parent().show();
     Object.keys(typeIsVisible).forEach(function(type) {
       typeIsVisible[type] = true;
     });
-    $('#gh-helper > ul > li.toggleLink').addClass('selected');
+    $ghHelperNavUl.find('li.toggleLink').addClass('selected');
   };
   self.hideAll = function() {
     $($allItems).parent().hide();
     Object.keys(typeIsVisible).forEach(function(type) {
       typeIsVisible[type] = false;
     });
-    $('#gh-helper > ul > li.toggleLink').removeClass('selected');
+    $ghHelperNavUl.find('li.toggleLink').removeClass('selected');
   };
+  self.addLegend();
   self.addLink('showAll', 'Show All');
   self.addLink('hideAll', 'Hide All');
   self.addDivider();
   var fileTypes = {};
-  $('div.meta[data-path]').each(function() {
-    var fileType = $(this).attr('data-path').split('.').pop();
+  var searchStr = 'div.meta[data-path]';
+  if (isPulls) {
+    // List of pull requests
+    searchStr = '.user-mention';
+  }
+  $(searchStr).each(function() {
+    var fileType = isPulls ? $(this).text().trim() : $(this).attr('data-path').split('.').pop();
     if (!fileTypes.hasOwnProperty(fileType)) {
       fileTypes[fileType] = [];
     }
@@ -70,6 +91,11 @@ function GhHelper(){
   });
   Object.keys(fileTypes).forEach(function(fileType) {
     self.addToggleLink(fileType);
+  });
+  $('#ghHelperNav > div > ul > li > .octicon').hover(function(){
+    $ghLegend.fadeIn();
+  }, function() {
+    $ghLegend.fadeOut();
   });
 }
 var ghHelper = new GhHelper();
